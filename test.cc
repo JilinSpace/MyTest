7e2dc668e963305361f9973fe716efea58ed4f37
ubuntu 64bit <ljl541461703@qq.com>
2017-03-21 17:55:15 +0800

    removed class AA in test.cc

--- test.cc
+++ test.cc
@@ -1,19 +1,8 @@
 #include <iostream>
 
-class AA
-{
-        public :
-                AA(){}
-                void output(){std::cout<<"feffff"<<std::endl;}
-};
-
 
 int main()
 {
-        AA a;
-        a.output();
-        a.output();
-	a.output();
 	for(int i=0; i<10; i++)
 	{
         std::cout<<i+2<<std::endl;
