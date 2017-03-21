6a63f521c9c2a9ca2394a910ea54c42b9a0dae52
ubuntu 64bit <541461703@qq.com>
2017-03-21 19:39:47 +0800

    c++ 11 std::bind class test

--- test.cc
+++ test.cc
@@ -9,11 +9,29 @@ void incre(int& a)
     a = a+100;
 }
 
+class Test
+{
+    public:
+        Test(){}
+        ~Test(){}
+
+        void show(int k)
+        {
+            std::cout<<"Test::show"<<k<<std::endl;
+        }
+};
+
+
+
 int main()
 {
     int a = 1;
     auto ft = std::bind(&incre, placeholders::_1);
     ft(a);
     std::cout<<a<<std::endl;
+
+    Test a2;
+    FT ft2 = std::bind(&Test::show, &a2, placeholders::_1);
+    ft2(232343);
 	return 0;
 }
