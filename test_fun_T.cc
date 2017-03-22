ab31d719789a072ea0ef43f7f1ec05f84d1d1c2c
ubuntu 64bit <541461703@qq.com>
2017-03-22 10:54:35 +0800

    std::function with a template type

---
+++ test_fun_T.cc
@@ -0,0 +1,50 @@
+#include <functional>
+#include <iostream>
+using namespace std;
+
+template<class T1, class T2 >
+T1 func(T1 x, T2 y)
+{
+        return x+y;
+}
+
+template<class T1, class T2>
+struct FunctionType
+{
+    typedef std::function<T1(T1, T2)> Type ;
+} ;
+
+
+template<class T1, class T2>
+class Test
+{
+public:
+    Test(){}
+    ~Test(){}
+
+    void create()
+    {
+        _temp_func = std::bind(&Test<T1, T2>::Show, this, placeholders::_1, placeholders::_2);
+    }
+
+    T1 Show(T1 x, T2 y)
+    {
+        std::cout<< x <<"\t"<< y <<std::endl;
+        return x;
+    }
+
+    typename FunctionType<T1, T2>::Type _temp_func;
+};
+
+int main()
+{
+        Test<int, char> t ;
+        t.create();
+        t._temp_func(1, 'z');
+        return 0;
+            
+        FunctionType<int, char>::Type Funcp = func<int, char> ;
+        int k = Funcp(1, 'a');
+        std::cout<<k<<"\t"<<char(k)<<std::endl;
+        return 0;
+}
